f6a01400aab625ef33faa6422387deb0d86c4173
root <root@ip-172-29-1-14.ap-southeast-1.compute.internal>
2018-11-28 07:47:43 +0000

    修改

--- install_zabbix_agent.sh
+++ install_zabbix_agent.sh
@@ -2,21 +2,28 @@
 
 #Date: 20180408
 #Author: Lile
-#Description: install zabbix agent
-#Contact: 15274326058
+#Version: V1.0.0
+#Description: 源码包的形式啊安装zabbix agent
+#Contact: 836217653@qq.com
 
 
 install_zabbix(){
 
+yum -y install pcre* gcc wget
+
+if [ ! -d $software_dir ];then
+    mkdir $software_dir
+fi
+
+cd $software_dir
+
 wget --no-check-certificate https://sourceforge.net/projects/zabbix/files/ZABBIX%20Latest%20Stable/3.4.7/zabbix-3.4.7.tar.gz
-  
-yum -y install pcre*
 
 tar -zxvf zabbix-3.4.7.tar.gz
 
 cd zabbix-3.4.7
 
-./configure --prefix=/usr/local/zabbix-3.4.7 --enable-agent
+./configure --prefix="$install_dir" --enable-agent
 make 
 make install
 
@@ -24,8 +31,8 @@ make install
 
 modify_file(){
 
-sed -i "s/^Server=127.0.0.1/Server=$1/g" /usr/local/zabbix-3.4.7/etc/zabbix_agentd.conf 
-sed -i "s/^ServerActive=127.0.0.1/ServerActive=$1/g" /usr/local/zabbix-3.4.7/etc/zabbix_agentd.conf 
+sed -i "s/^Server=127.0.0.1/Server=$1/g" $install_dir/etc/zabbix_agentd.conf 
+sed -i "s/^ServerActive=127.0.0.1/ServerActive=$1/g" $install_dir/etc/zabbix_agentd.conf 
 
 }
 
@@ -42,9 +49,9 @@ fi
 
 add_service_start(){
 
-cp /root/zabbix-3.4.7/misc/init.d/fedora/core/zabbix_agentd  /etc/init.d/zabbix_agentd
+cp $software_dir/zabbix-3.4.7/misc/init.d/fedora/core/zabbix_agentd  /etc/init.d/zabbix_agentd
 
-sed -i 's!BASEDIR=/usr/local!BASEDIR=/usr/local/zabbix-3.4.7!g' /etc/init.d/zabbix_agentd
+sed -i "s!BASEDIR=/usr/local!BASEDIR=$install_dir!g" /etc/init.d/zabbix_agentd
 
 }
 
@@ -57,7 +64,14 @@ service zabbix_agentd start
 
 main(){
 
+#指定zabbix server端地址
 zabbix_server="192.168.43.203"
+
+#指定软件下载路径
+software_dir=/software
+
+#指定软件安装路径
+install_dir=/usr/local/zabbix
 
 install_zabbix
 modify_file $zabbix_server
