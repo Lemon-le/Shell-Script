749c24e0cff0f6dca8643058b3a744a4be10174c
LiLe <le.li@transsnet.com>
2018-12-14 17:15:15 +0800

    添加rpm包的方式安装zabbix agent

---
+++ zabbix_agent_rpm.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#Date: 20181214
+#Author: Lile
+#Version: V1.0.0
+#Description: RPM包的形式啊安装zabbix agent
+#Contact: 836217653@qq.com
+#Zabbix web: https://www.zabbix.com/download
+
+install(){
+
+rpm -i https://repo.zabbix.com/zabbix/4.0/rhel/7/x86_64/zabbix-release-4.0-1.el7.noarch.rpm
+yum -y install zabbix-agent
+
+}
+
+modify_file(){
+
+sed -i "s/^Server=127.0.0.1/Server=$1/g" /etc/zabbix/zabbix_agentd.conf 
+sed -i "s/^ServerActive=127.0.0.1/ServerActive=$1/g" /etc/zabbix/zabbix_agentd.conf 
+
+}
+
+start_zabbix(){
+
+systemctl status zabbix-agent
+
+}
+
+server_address=10.0.0.15
+
+main(){
+
+install
+modify_file $server_address
+start_zabbix
+
+}
+
+main
+
