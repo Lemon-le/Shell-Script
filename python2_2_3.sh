661bc9eaa0e614c02c95f6190bb73553413534a9
LiLe <le.li@transsnet.com>
2019-05-14 17:26:59 +0800

    添加centos7下安装python3的脚本

---
+++ python2_2_3.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#FileName: python2_2_3.sh
+#Date: 20190514
+#Author: LiLe
+#Contact: 836217653@qq.com
+#Version: V1.0
+#Description: Centos7下python2升级成Python3
+
+download(){
+    wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
+    xz -d Python-3.7.3.tar.xz
+    tar -xvf Python-3.7.3.tar
+}
+
+compile(){
+    yum install -y libffi libffi-devel
+    cd Python-3.7.3
+    ./configure --prefix=/usr/local/python3
+    make && make install
+}
+
+create_link(){
+    ln -s /usr/local/python3/bin/python3.7 /usr/bin/python3
+    python3 --version
+}
+
+main(){
+    download
+    compile
+    create_link
+}
+
+main
